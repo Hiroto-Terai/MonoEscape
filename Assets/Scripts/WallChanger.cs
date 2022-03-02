990e6fd2f6420efbdc9e0a82d4c461ec14dcc6f7
Hiroto-Terai <hiro08051610@gmail.com>
2022-03-02 14:52:34 +0900

    update: 全ての画面とフレームを追加

--- Assets/Scripts/WallChanger.cs
+++ Assets/Scripts/WallChanger.cs
@@ -10,12 +10,12 @@ public class WallChanger : MonoBehaviour
     Wall1,
     Wall2,
     Wall3,
-    Wall4,
-    Wall5,
-    Wall6,
-    Wall7,
-    Wall8,
-    Wall9,
+    Wall00,
+    Wall01,
+    Wall02,
+    Wall03,
+    Wall04,
+    Wall05,
   }
 
   Wall currentWall;
@@ -41,8 +41,6 @@ public class WallChanger : MonoBehaviour
 
   public void OnLeftButton()
   {
-    Debug.Log("left");
-
     switch (currentWall)
     {
       case Wall.Wall0:
@@ -62,12 +60,17 @@ public class WallChanger : MonoBehaviour
 
   public void OnBackButton()
   {
-
+    switch (currentWall)
+    {
+      case Wall.Wall00:
+        Show(Wall.Wall0);
+        break;
+    }
   }
 
   public void OnSample()
   {
-    Show(Wall.Wall4);
+    Show(Wall.Wall00);
   }
 
   void Show(Wall Wall)
@@ -89,7 +92,7 @@ public class WallChanger : MonoBehaviour
       case Wall.Wall3:
         transform.localPosition = new Vector2(-3000, 0);
         break;
-      case Wall.Wall4:
+      case Wall.Wall00:
         transform.localPosition = new Vector2(0, 2000);
         break;
         // フォーカスの画像
