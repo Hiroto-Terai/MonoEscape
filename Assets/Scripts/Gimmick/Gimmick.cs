7125ee850058388aaeb1f26dc3dc84a638a61224
Hiroto-Terai <hiro08051610@gmail.com>
2022-03-12 20:45:53 +0900

    fix: 壁ギミックのスクリプトを汎用的なギミックスクリプトに変更

--- Assets/Scripts/Gimmick/Gimmick.cs
+++ Assets/Scripts/Gimmick/Gimmick.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Wall : MonoBehaviour
+public class Gimmick : MonoBehaviour
 {
   [SerializeField] Item.Type clearItemType;
   [SerializeField] GameObject Crashed;
