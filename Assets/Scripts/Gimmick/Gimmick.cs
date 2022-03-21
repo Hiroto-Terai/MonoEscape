1791496bf1b09268d010769d68bd40485c380ef8
Hiroto-Terai <hiro08051610@gmail.com>
2022-03-21 19:39:35 +0900

    update: ハンマー取得までのギミック実装

--- Assets/Scripts/Gimmick/Gimmick.cs
+++ Assets/Scripts/Gimmick/Gimmick.cs
@@ -4,20 +4,32 @@ using UnityEngine;
 
 public class Gimmick : MonoBehaviour
 {
-    [SerializeField] Item.Type clearItemType;
-    [SerializeField] GameObject crashedWall;
+    [SerializeField] Item.Type clearItemType = default;
+    [SerializeField] GameObject crashedWall = null;
     Transform TransForm;
+
+    // ゴミ箱
     private bool isUpTrashCan = false;
+
+    // カフェラテ
     private bool isUpCaffelate = false;
+
+    // 金庫
+    [SerializeField] GameObject[] blackPanels = null;
+    [SerializeField] GameObject safeOpened = null;
+    GameObject blackMass;
+    bool isClick;
+
+    // レバーBOX
+    [SerializeField] GameObject[] reversies = null;
+    [SerializeField] GameObject switchBoxOpened = null;
+    GameObject reversi;
 
     private void Start()
     {
         TransForm = GetComponent<Transform>();
     }
 
-    // ゴミ箱
-    // クリックすると上に上がる
-    // もう一度クリックすると元の位置に戻る
     public void OnClickTrashCan()
     {
         if (!isUpTrashCan)
@@ -30,9 +42,6 @@ public class Gimmick : MonoBehaviour
         TransForm.localPosition = new Vector2(111, 165);
     }
 
-    // カフェラテ
-    // クリックすると上に上がる
-    // もう一度クリックすると元の位置に戻る
     public void OnClickCafelatte()
     {
         if (!isUpCaffelate)
@@ -44,23 +53,50 @@ public class Gimmick : MonoBehaviour
         isUpCaffelate = false;
         TransForm.localPosition = new Vector2(0, 182);
     }
-
-    // 金庫のパスワードギミック
-    // マスを押すと黒いマスが表示される
-    // もう一度マスを押すと黒が非表示になる
-    // 黒->白->黒->黒の順(後で修正?)にすると前の画面で金庫のOpenedが表示される
-    public void SafePassword()
+    public void SafeMassChangeBlack()
     {
+        // 押すと子オブジェクトの黒マス表示
+        blackMass = transform.GetChild(0).gameObject;
+        blackMass.SetActive(true);
+    }
 
+    public void SafeMassChangeWhite()
+    {
+        // 黒マスを押すと非表示
+        this.gameObject.SetActive(false);
+    }
+
+    public void OnClickSafeEnter()
+    {
+        // 黒->白->黒->黒の順(後で修正?)にすると前の画面で金庫のOpenedが表示される
+        if (blackPanels[0].activeSelf && !blackPanels[1].activeSelf
+        && blackPanels[2].activeSelf && blackPanels[3].activeSelf)
+        {
+            safeOpened.SetActive(true);
+        }
     }
 
     // レバーBOXギミック
     // リバーシを選択した状態で型を押すとはまる
     // アイテムBOXのリバーシが消える
     // 3つはまると前の画面で金庫のOpenedが表示される
-    public void OnLeverBox()
+    public void OnLeverBoxSwitch()
     {
+        bool isOkUseReversi = ItemBox.instance.TryUseItem(clearItemType);
+        if (isOkUseReversi)
+        {
+            reversi = transform.GetChild(0).gameObject;
+            reversi.SetActive(true);
+        }
+    }
 
+    public void OnLeber()
+    {
+        if (reversies[0].activeSelf && reversies[1].activeSelf
+        && reversies[2].activeSelf)
+        {
+            switchBoxOpened.SetActive(true);
+        }
     }
 
     // 壁壊すギミック
@@ -69,8 +105,8 @@ public class Gimmick : MonoBehaviour
     public void OnClickWall()
     {
         // アイテムHammerを持っているかどうか
-        bool clear = ItemBox.instance.TryUseItem(clearItemType);
-        if (clear)
+        bool isOkUseHammer = ItemBox.instance.TryUseItem(clearItemType);
+        if (isOkUseHammer)
         {
             // 壊れる前の壁を削除
             gameObject.SetActive(false);
